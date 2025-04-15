fc73e4c9e4ab6a5a6f5fa9d88765654a63cb7a2d
Barton Chittenden <bartonski@gmail.com>
2025-04-15 19:49:27 -0400

    Add contours layer.

--- cv2_juggling.py
+++ cv2_juggling.py
@@ -3,24 +3,32 @@ import sys
 from core.detector import Detector
 from core.orientation import Orientation
 from layers.pose_detection.pose_detector import PoseDetector
+from layers.contours.contours import Contours
 
 def main():
     filename = str(sys.argv[-1])
-    detector_config = {
-        'detector_history': 100,
-        'detector_threshold': 40,
-        'area_threshold': 50,
-        'grey_threshold': 255
-    }
-    pose_detector_config = {
-        'model_asset_path': 'layers/pose_detection/pose_landmarker_heavy.task'
+    config = {
+        'detector':{
+            'detector_history': 100,
+            'detector_threshold': 40,
+            'area_threshold': 50,
+            'grey_threshold': 255
+        }
+        , 'pose_detector':{
+            'model_asset_path': 'layers/pose_detection/pose_landmarker_heavy.task'
+        }
+        , 'contours':{
+            'outline_color': (0,255,0)
+            , 'outline_thickness': 2
+        }
     }
     # should populate orientation_config, pass to Orientation
 
     cap = cv2.VideoCapture(filename)
     orientation = Orientation(filename)
-    jd = Detector( detector_config ) 
-    pose = PoseDetector( pose_detector_config )
+    jd = Detector(config.get('detector')) 
+    pose = PoseDetector( config.get('pose_detector'))
+    cntrs = Contours( config.get('contours'))
 
     cv2.namedWindow("Juggling")
     cv2.namedWindow("Mask")
@@ -33,6 +41,7 @@ def main():
     while ret:
         mask = jd.mask( frame )
         pose.get_pose( frame )
+        frame = cntrs.draw( frame, { 'contours':jd.contours(mask) } )
         cv2.imshow("Juggling", frame)
         cv2.imshow("Mask", pose.draw( mask ) ) 
 
---
+++ layers/contours/contours.py
@@ -0,0 +1,72 @@
+import cv2
+
+from core.orientation import Orientation
+from core.detector import Detector
+
+class Contours:
+
+    def __init__(self,config):
+        self._outline_color=config['outline_color']
+        self._outline_thickness=config['outline_thickness']
+
+    def draw(self,image,arguments=None):
+        cv2.drawContours(
+            image                       # image to draw on
+            , arguments.get('contours') # list of contours to draw
+            , -1                        # index of contour to draw (-1 -> all contours)
+            , self._outline_color       # color of outline
+            , self._outline_thickness   # thickness
+        )
+        return image 
+
+def main():
+    config = {
+        'detector':{
+            'detector_history': 100,
+            'detector_threshold': 40,
+            'area_threshold': 50,
+            'grey_threshold': 255
+        }
+        , 'contours':{
+            'outline_color': (0,255,0)
+            , 'outline_thickness': 2
+        }
+    }
+    filename = str(sys.argv[-1])
+
+    cap = cv2.VideoCapture(filename)
+
+    jd = Detector( config.get('detector') )
+    cntrs = Contours( config.get('contours') )
+
+    cv2.namedWindow("Contours")
+
+    ret, frame = cap.read()
+    # ret = True
+
+    while ret:
+        # args contains data passed to draw
+        mask = jd.mask( frame )
+        args = { contours:jd.contours(mask) }
+        cv2.imshow("Contours", contours.draw( frame, args ) )
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        ret, frame = cap.read()
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
+
+# cv2.drawContours(
+#     image               # image to draw on
+#     , contours          # list of contours to draw
+#     , -1                # index of contour to draw (-1 -> all contours)
+#     , (0, 255, 0)       # color of outline
+#     , 2                 # thickness
+# )
